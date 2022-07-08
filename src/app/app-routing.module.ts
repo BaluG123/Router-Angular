b6ddbdb8541adeebb84d7a20796440fd08b19990
balu <balu1072000@gmail.com>
2022-07-08 11:55:28 +0530

    Router Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'aboutus',
+    component:AboutComponent
+  },
+  {
+    path:'user/:id',
+    component:UserComponent
+  },
+  {
+    path:'privacy',
+    component:PrivacyComponent
+  },
+  {
+    path:'sports',
+    component:SportsComponent
+  },
+  {
+    path:'entertainment',
+    component:EntertainmentComponent
+  },
+  {
+    path:'technology',
+    component:TechnologyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { SportsComponent } from './sports/sports.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    UserComponent,
+    PrivacyComponent,
+    SportsComponent,
+    TechnologyComponent,
+    EntertainmentComponent
   ],
   imports: [
     BrowserModule,
